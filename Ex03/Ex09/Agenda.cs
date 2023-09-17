17f1ca8f954f22bb62382e2c3e88eaac41944254
patrick-e404 <patrick_e404@hotmail.com>
2023-09-17 10:42:53 -0300

    Agenda

---
+++ Ex03/Ex09/Agenda.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex09
+{
+    public class Agenda
+    {
+        private string[] contatos;
+        private string[] telefones;
+        private int espacoAgenda;
+
+        public Agenda()
+        {
+            contatos = new string[5];
+            telefones = new string[5];
+            espacoAgenda = 0;
+        }
+        public void AdicionarContato(string nome, string telefone)
+        {
+            if (espacoAgenda < 5)
+            {
+                contatos[espacoAgenda] = nome;
+                telefones[espacoAgenda] = telefone;
+                espacoAgenda++;
+            }
+            else
+            {
+                Console.WriteLine("Agenda está cheia.");
+            }
+        }
+        public void ListarContatos()
+        {
+            Console.WriteLine("Agenda:");
+            for (int i = 0; i < espacoAgenda; i++)
+            {
+                Console.WriteLine($"Nome:  {contatos[i]}, telefone: {telefones[i]}");
+            }
+        } 
+    }
+}
---
+++ Ex03/Ex09/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex09
+{
+    public class Program
+    {
+        Agenda minhaAgenda;
+        public Program() 
+        { 
+            Agenda minhaAgenda = new Agenda();
+            minhaAgenda.AdicionarContato("Maria", "123-456-7890");
+            minhaAgenda.AdicionarContato("João", "9877-654-3210");
+            minhaAgenda.ListarContatos();
+        }
+
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+            Console.ReadKey();
+        }
+    }
+}
