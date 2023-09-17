9a666b1e51a98d8ad7ed15ed381b207bede76a83
patrick-e404 <patrick_e404@hotmail.com>
2023-09-17 09:18:14 -0300

    Retangulo

---
+++ Ex03/Ex05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05
+{
+    public class Program
+    {
+        Retangulo meuRetorno;
+        public Program()
+        {
+            meuRetorno = new Retangulo(5.0, 3.0);
+            double area = meuRetorno.CalcularArea();
+        }
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ex03/Ex05/Retangulo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05
+{
+    public class Retangulo
+    {
+        private double largura;
+        private double altura;
+
+        public Retangulo()
+        {
+            largura = 0;
+            altura = 0;
+        }
+        public Retangulo(double largura, double altura)
+        {
+            this.largura = largura;
+            this.altura = altura;
+        }
+        public double Largura
+        {
+            get { return largura; }
+            set { largura = value; }
+        }
+        public double Altura
+        {
+            get { return altura; }
+            set {  altura = value; }
+        }
+        public double CalcularArea()
+        {
+            double area = largura * altura;
+            Console.WriteLine($"A area é: {area}m².");
+
+            return area;
+        }
+    }
+}
