e7a2094e62477ab510c8366d32ff9e910108cd69
patrick-e404 <patrick_e404@hotmail.com>
2023-09-17 10:11:29 -0300

    Produto

---
+++ Ex03/Ex07/Poduto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07
+{
+    public class Produto
+    {
+        private string nome;
+        private double preco;
+        private int quantidadeEmEstoque;
+
+        public Produto()
+        {
+            nome = string.Empty;
+            preco = 0;
+            quantidadeEmEstoque = 0;
+        }
+        public Produto(string nome, double preco, int quantidadeEmEstoque)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidadeEmEstoque = quantidadeEmEstoque;
+        }
+        public string Nome
+        {
+            get { return nome; } 
+            set { nome = value; }
+        }
+        public double Preco
+        {
+            get { return preco; }
+            set { preco = value; }
+        }
+        public int QuantidadeEmEstoque
+        {
+            get { return quantidadeEmEstoque; }
+            set { quantidadeEmEstoque = value; }
+        }
+        public void AdicinonarEstoque(int valor)
+        {
+            quantidadeEmEstoque += valor;
+            Console.WriteLine($"Quantidade em estoque após adicionar: {QuantidadeEmEstoque}");
+        }
+        public void RemoverEstoque(int valor)
+        {
+            quantidadeEmEstoque -= valor;
+            Console.WriteLine($"Quantidade em estoquee após remoção: {quantidadeEmEstoque}");
+        }
+        public void DadosProdutos()
+        {
+            Console.WriteLine($"Produto; {Nome}");
+            Console.WriteLine($"Preço: {Preco}");
+            Console.WriteLine($"Quantidade inicial em estoque: {QuantidadeEmEstoque}");
+        }
+    }
+}
---
+++ Ex03/Ex07/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07
+{
+    public class Program
+    {
+        Produto produto = new Produto();
+        public Program() 
+        { 
+            Produto produto1 = new Produto("Camiseta", 20, 50);
+            produto1.DadosProdutos();
+            produto1.AdicinonarEstoque(10);
+            produto1.RemoverEstoque(5);
+        }
+
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+            Console.ReadKey();
+        }
+    }
+}
