990d1686b33371a5febff604a5353b91bab05fe4
patrick-e404 <patrick_e404@hotmail.com>
2023-09-17 10:49:11 -0300

    Dado

---
+++ Ex03/Ex10/Dado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex10
+{
+    public class Dado
+    {
+        private Random random;
+        public Dado() 
+        { 
+            random = new Random();
+        }
+        public int RolarDado()
+        {
+            int resultado = random.Next(1, 7);
+            {
+                return  resultado;
+            }
+        }
+    }
+}
---
+++ Ex03/Ex10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex10
+{
+    public class Program
+    {
+        Dado meuDado;
+        public Program()
+        {
+            Dado meuDado = new Dado();
+            int resultado = meuDado.RolarDado();
+            Console.WriteLine("Valor no Dado:");
+            Console.WriteLine(resultado);
+        }
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+            Console.ReadKey();
+        }
+    }
+}
