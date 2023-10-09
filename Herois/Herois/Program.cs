91cf7aa8082034f9b79e5005a8ddac647c503644
patrick-e404 <patrick_e404@hotmail.com>
2023-10-09 09:56:14 -0300

    Heroi01

---
+++ Herois/Herois/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herois
+{
+    public class Program
+    {
+        SuperPoder superPoder;
+        public Program()
+        {
+            superPoder = new SuperPoder("Sentido Apurado", 1);
+
+            superPoder.Exibir();
+        }
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Herois/Herois/SuperPoder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herois
+{
+    internal class SuperPoder
+    {
+        private string nome;
+        private int categoria;
+
+        public SuperPoder()
+        {
+            nome = "";
+            categoria = 0;
+        }
+
+        public SuperPoder(string nome, int categoria)
+        {
+            this.nome = nome;
+            this.categoria = categoria;
+        }
+
+        public string getNome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public int getCategoria
+        {
+            get { return  categoria; }
+            set { categoria = value; }
+        }
+        public void Exibir()
+        {
+            Console.WriteLine($"O nome do poder é: {nome}");
+            Console.WriteLine($"A categoria do poder é: {categoria}");
+        }
+    }
+}
